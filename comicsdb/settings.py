f91757df56b400e3919e15991f377d66a7d79ba0
nonameitem <nonameitem@me.com>
2019-03-20 02:30:13 +0300

    DO static files url

--- comicsdb/settings.py
+++ comicsdb/settings.py
@@ -141,6 +141,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 STATICFILES_STORAGE = custom_settings.STATICFILES_STORAGE
 
